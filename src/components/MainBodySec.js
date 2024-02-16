af19412ff454872793ba06928371ec98648bfaea
AbrarDhukka <abrdhk@gmail.com>
2024-02-16 21:10:56 +0530

    Updated

--- src/components/MainBodySec.js
+++ src/components/MainBodySec.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_API } from "../utils/const";
+import VideoCard from "./VideoCard";
 
 const MainBodySec = () => {
-  return (
-    <div>MainBodySec</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
 
-export default MainBodySec
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_API);
+    const json = await data.json();
+    // console.log(json)
+    setVideos(json.items);
+  };
+  return (
+    <>
+    <div className="w-[85%] flex flex-wrap justify-center items-center">
+    {videos.map(video => <VideoCard key={video.id} info={video}></VideoCard>)}
+    </div>
+    </>
+  );
+};
+
+export default MainBodySec;
