c993588f2a997b3bf14916b371710505d5123818
AbrarDhukka <abrdhk@gmail.com>
2024-02-17 22:35:34 +0530

    Updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import MainBodySec from './MainBodySec'
+import React from "react";
+import Sidebar from "./Sidebar";
+import MainBodySec from "./MainBodySec";
 
 const Body = () => {
   return (
-    <div className='flex'>
-         <Sidebar></Sidebar>
-         <MainBodySec></MainBodySec>
+    <div className="flex">
+      <Sidebar></Sidebar>
+      <MainBodySec></MainBodySec>
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
