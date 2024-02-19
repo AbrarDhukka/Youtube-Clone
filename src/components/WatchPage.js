2cd0fb598cb5480f7e6024c3cf57ed7521597ccb
AbrarDhukka <abrdhk@gmail.com>
2024-02-19 22:38:16 +0530

    Updated

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -6,12 +6,25 @@ import { useSearchParams } from "react-router-dom";
 const WatchPage = () => {
   const dispatch = useDispatch();
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get("v"))
+  console.log(searchParams.get("v"));
 
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
-  return <div>WatchPage</div>;
+  return (
+    <div className="px-5 fixed z-10">
+      <iframe
+      className="rounded-2xl"
+        width="1000"
+        height="500"
+        src={"https://www.youtube.com/embed/"+searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
