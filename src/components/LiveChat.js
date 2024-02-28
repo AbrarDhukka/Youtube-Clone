d1d3529cf5ed2377ca1b5608f588e36558e0fbac
AbrarDhukka <abrdhk@gmail.com>
2024-02-28 22:43:54 +0530

    Updated

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const LiveChat = () => {
   return (
-    <div>LiveChat</div>
+    <div className='border-2 border-black ml-2 p-2 w-[450px] h-[500px] rounded-lg bg-slate-100'>
+    LiveChat
+    </div>
   )
 }
 
