03afb47b99ae07cf8c8b8363c7b9a387eab13674
AbrarDhukka <abrdhk@gmail.com>
2024-02-28 23:17:29 +0530

    Updated

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const ChatMessage = () => {
   return (
-    <div className='flex items-center p-2'>
-        <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM_1baO4t0RgM8tpHYBW_Ue7aiZUOhNoNP5Q&usqp=CAU"
-                alt="profile"
-                className="h-7"
-              />
-              <span className='font-bold'>Abrar Dhukka</span>
-              <span className='px-2'> Hey Abrar here</span>
+    <div className="flex items-center p-2">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM_1baO4t0RgM8tpHYBW_Ue7aiZUOhNoNP5Q&usqp=CAU"
+        alt="profile"
+        className="h-7"
+      />
+      <span className="font-bold">Abrar Dhukka</span>
+      <span className="px-2"> Hey Abrar here</span>
     </div>
-  )
-}
+  );
+};
 
-export default ChatMessage
+export default ChatMessage;
