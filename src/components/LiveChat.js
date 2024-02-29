3bfe790e27fb5c4f706a6fd43ed443c7ee79f5a2
AbrarDhukka <abrdhk@gmail.com>
2024-02-29 22:46:39 +0530

    Updated

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import ChatMessage from './ChatMessage'
+import React from "react";
+import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
   return (
-    <div className='border-2 border-black ml-2 p-2 w-[450px] h-[500px] rounded-lg bg-slate-100'>
-    <ChatMessage></ChatMessage>
-    <ChatMessage></ChatMessage>
-    <ChatMessage></ChatMessage>
-    <ChatMessage></ChatMessage>
-    <ChatMessage></ChatMessage>
-    <ChatMessage></ChatMessage>
+    <div className="border-2 border-black ml-2 p-2 w-[450px] h-[500px] rounded-lg bg-slate-100">
+      <ChatMessage></ChatMessage>
+      <ChatMessage></ChatMessage>
+      <ChatMessage></ChatMessage>
+      <ChatMessage></ChatMessage>
+      <ChatMessage></ChatMessage>
+      <ChatMessage></ChatMessage>
     </div>
-  )
-}
+  );
+};
 
-export default LiveChat
+export default LiveChat;
