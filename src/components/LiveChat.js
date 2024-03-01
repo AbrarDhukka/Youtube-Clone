7b82dd1292eaa64aa9c31298d57833906151144e
AbrarDhukka <abrdhk@gmail.com>
2024-03-01 22:35:05 +0530

    Updated

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -2,6 +2,8 @@ import React from "react";
 import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
+
+
   return (
     <div className="border-2 border-black ml-2 p-2 w-[450px] h-[500px] rounded-lg bg-slate-100">
       <ChatMessage name="Abrar Dhukka" message="Hey, I am writing in live chat"></ChatMessage>
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import headReducer from "./headSlice";
+import chatSlice from "./chatSlice";
 
 export const store = configureStore({
   reducer: {
     head: headReducer,
+    chat: chatSlice
   },
 });
