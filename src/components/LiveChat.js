4eab5abe24652dd2edc1071023e7e777e3e6a158
AbrarDhukka <abrdhk@gmail.com>
2024-03-05 21:13:20 +0530

    Updated

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -10,7 +10,7 @@ const LiveChat = () => {
     <div className="border-2 border-black ml-2 p-2 w-[450px] h-[500px] rounded-lg bg-slate-100">
       <ChatMessage
         name="Abrar Dhukka"
-        message="Hey, I am writing in live chat"
+        message="Hey, I am writing in live chat."
       ></ChatMessage>
     </div>
   );
